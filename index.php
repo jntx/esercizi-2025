e5839347e4cf4f5d54c1fb36b82ef52f272cdac2
Jenner Fusari <jfusari@coopolis.it>
2025-10-16 08:08:18 +0200

    Aggiunti placeholder e strutturazione a righe

--- index.php
+++ index.php
@@ -8,13 +8,35 @@
 <body>
 	<form action="<?=$_SERVER["PHP_SELF"]?>">
 		<input type="hidden" name="section" value="<?=basename($_SERVER["PHP_SELF"])?>">
-		<input type="text" name="nome">
-		<input type="text" name="cognome">
+
+        <div>
+            <label>
+            Nome:<br>
+		<input type="text" name="nome" placeholder="inserisci il nome"><br><br>
+            </label>
+        </div>
+
+        <div>
+            <label>
+            Cognome:<br>
+		<input type="text" name="cognome" placeholder="inserisci il cognome"><br><br>
+            </label>
+        </div>
+        
+        <div>
+            <label>
+            Action:<br>
 		<select name="action">
 			<option value="add">Aggiungi persona</option>
 			<option value="delete">Rimuovi persona</option>
 		</select>
+           </label>
+       </div>
+       <br>
+
+       <div>
 		<input type="submit" value="Esegui">
+       </div>
 	</form>
 </body>
 </html>
