25068bd35c11a108a0a77efe4cd7fdfd473cd5b3
Sagf10 <oyerindesaheed100@gmail.com>
2025-11-23 19:27:16 +0100

    ID duplicati

--- add.php
+++ add.php
@@ -1,9 +1,9 @@
 <?php
 header('Content-Type: application/json');
 
-$new_nome = $_POST['nome'] ?? $_GET['nome'] ?? null;
-$new_cognome = $_POST['cognome'] ?? $_GET['cognome'] ?? null;
-$new_id = $_POST['id'] ?? $_GET['id'] ?? null;
+$new_nome = array_key_exists('nome', $_REQUEST) ? $_REQUEST['nome'] : null;
+$new_cognome = array_key_exists('cognome', $_REQUEST) ? $_REQUEST['cognome'] : null;
+$new_id = array_key_exists('id', $_REQUEST) ? $_REQUEST['id'] : null;
 
 if (!$new_nome || !$new_cognome || !$new_id) {
     echo json_encode(["success" => false, "message" => "Per favore, inserisci informazioni obbligatori!"]);
@@ -13,7 +13,22 @@ if (!$new_nome || !$new_cognome || !$new_id) {
 $file = 'Soggetti.json';
 $data = json_decode(file_get_contents($file), true);
 
+$Takenid = [];
+foreach ($data as $soggetto) {
+    $Takenid[] = $soggetto["ID"];
+}
+
+while (in_array($new_id, $Takenid)) {
+    echo json_encode([
+        "success" => false,
+        "message" => "ID già esistente, scegli un altro!"
+    ]);
+    exit;
+}
+
 $data[] = ["nome" => $new_nome, "cognome" => $new_cognome,"ID"=> $new_id];
+
+
 
 file_put_contents($file, json_encode($data, JSON_PRETTY_PRINT));
 
